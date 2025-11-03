2dd3ae66ff00d5c6dfe3e7c7f164abc0a100205d
vijaysinh Patil <vijaysinhpatil28@gmail.com>
2025-11-03 08:43:27 +0530

    ts codes as begineer

---
+++ Part2/index.md
@@ -0,0 +1,7 @@
+
+# what i did 
+
+- write a program of addition of two number
+- error handling using type annotations
+- Ts configuration file installation
+
---
+++ Part2/index.ts
@@ -0,0 +1,16 @@
+// Write a Program to add Two Numbers
+
+// @ts-ignore
+const addition = (a:number , b:number) : number => {
+
+    let c : number = a + b
+    return c
+}
+
+// @ts-ignore
+let a : number = 10
+
+// @ts-ignore
+let b : number = "Vijaysinh"
+
+console.log("addition of Two numbers is => " , addition(a,b))
